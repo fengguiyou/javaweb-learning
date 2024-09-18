af0abe3529676ad52c8da6b6e79cfda6691b91a2
unknown <2795833307@qq.com>
2024-09-18 17:18:18 +0800

    servlet 初体验

---
+++ class01-servlet/src/main/java/top/soft/class01servlet/ServletDemo3.java
@@ -0,0 +1,60 @@
+package top.soft.class01servlet;
+
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+@WebServlet(value = "/demo3",name = "demo3")
+public class ServletDemo3  implements Servlet {
+    /**
+     *
+     * @param servletConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("ServletDemo3 初始化");
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    /**
+     *
+     * @param servletRequest
+     * @param servletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.getWriter().write("servletDemo3 执行");
+        System.out.println("service 执行");
+    }
+
+    /**
+     *获取Servlet信息
+     *
+     * @return
+     */
+    @Override
+    public String getServletInfo() {
+        return "";
+    }
+
+    /**
+     * 执行销毁方法
+     */
+    @Override
+    public void destroy() {
+        System.out.println("销毁方法");
+    }
+}
